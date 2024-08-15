e1bb4298a9ad7237e96b93d717e528225626cfd5
quintas <caioquintassantiago@gmail.com>
2024-08-15 20:22:11 -0300

    refactor express adapter

--- src/layers/infra/adapters/expressAdapter.ts
+++ src/layers/infra/adapters/expressAdapter.ts
@@ -1,12 +1,14 @@
 import {HttpProvider, Method} from "../../../lib/contracts/httpProvider";
 import express, {Express, Request, Response, Router} from "express"
 import * as http from "node:http";
-import {Controller} from "../../../lib/presentation/controller";
+import {Controller, HttpResponse} from "../../../lib/presentation/controller";
 import {Logger} from "../../../lib/contracts/logger";
 import compression from "compression";
 import cors from "cors"
 import helmet from 'helmet';
 import bodyParser from "body-parser";
+import {logger} from "../connections/logger";
+import {HttpError, internalServerError} from "../../../lib/presentation/errors";
 
 export const corsOptions = {
     origin: '*',
@@ -56,8 +58,39 @@ export class ExpressAdapter extends HttpProvider<Router> {
         })
     }
 
+    private async baseHandler(req: Request, res: Response, controller: Controller<any, any>) {
+        let result: HttpResponse<any> | undefined
+
+        try {
+            result = await controller.handle({body: req.body})
+        } catch (err: unknown) {
+            logger.error(err as Error)
+            if (err instanceof HttpError)
+                return this.handleError(err, res)
+            return this.handleError(internalServerError, res)
+        }
+
+        if (!result)
+            return this.handleError(internalServerError, res)
+
+        switch (result.contentType) {
+            case 'json':
+                this.jsonResponse(result.statusCode ?? 200, result.body, res)
+        }
+    }
+
+    private handleError(error: HttpError<any>, res: Response) {
+        this.jsonResponse(error.statusCode, error.body, res)
+    }
+
+    private jsonResponse<T>(status: number, body: T, res: Response) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(status)
+        res.json(body)
+    }
+
     route(router: Router, method: Method, path: string, controller: Controller<any, any>): void {
-        router[method](path, [], controller.handle)
+        router[method](path, [], (req: Request, res: Response) => this.baseHandler(req, res, controller))
     }
 
     close(): void {
--- src/lib/presentation/errors.ts
+++ src/lib/presentation/errors.ts
@@ -1,18 +1,28 @@
 import {BaseError, ErrorEnum} from "../errors/errors";
 
 export class HttpError<T> extends BaseError {
-    private statusCode: number
-    private body: T
+    private readonly _statusCode: number
+    private readonly _body: T
 
-    constructor(message: string, statusCode: number, body?: T) {
+    get statusCode() {
+        return this._statusCode
+    }
+
+    get body() {
+        return this._body
+    }
+
+    constructor(message: string, _statusCode: number, _body?: T) {
         super(message, ErrorEnum.PRESENTATION);
-        const flatBody = body ? body : {}
-        this.body = {
-            ...flatBody as T,
+        const flat_body = _body ? _body : {}
+        this._body = {
+            ...flat_body as T,
             message
         }
-        this.statusCode = statusCode
+        this._statusCode = _statusCode
     }
 }
 
 export const badRequest = new HttpError('Bad request', 400)
+
+export const internalServerError = new HttpError('Internal server error', 500)
